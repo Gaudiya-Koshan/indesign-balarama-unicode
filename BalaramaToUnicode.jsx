c27b6da53fe7ef23f98e1c95fefa42b96bed6c3f
ekendra-nz <le.pattillo.nz@gmail.com>
2023-05-21 10:17:04 +1200

    init

---
+++ BalaramaToUnicode.jsx
@@ -0,0 +1,115 @@
+
+// object with unicode compliant characters
+var unicode = {
+	1: 'ā', // _a
+	2: 'ī', // _i
+	3: 'ū', // _u
+	4: 'ṛ', // .r
+	5: 'ṝ', // _.r
+	6: 'ṅ', // 'n
+	7: 'ñ', // ~n
+	8: 'ṭ', // .t
+	9: 'ḍ', // .d
+	10: 'ṇ', // .n
+	11: 'ś', // 's
+	12: 'ṣ', // .s
+	13: 'ṁ', // 'm (anusvara)
+	14: 'ḥ', // .h (visarga)
+	15: 'ḷ', // .l
+	16: 'ḹ', // _.l
+
+	20: 'ḏ', // _d
+	21: 'ẏ', // .y
+
+	30: 'Ā', // _a
+	31: 'Ī', // _i
+	32: 'Ū', // _u
+	33: 'Ṛ', // .r
+	34: 'Ṝ', // _.r
+	35: 'Ṅ', // 'n
+	36: 'Ñ', // ~n
+	37: 'Ṭ', // .t
+	38: 'Ḍ', // .d
+	39: 'Ṇ', // .n
+	40: 'Ś', // 's
+	41: 'Ṣ', // .s
+	42: 'Ṁ', // 'm (anusvara)
+	43: 'Ḥ', // .h (visarga)
+	44: 'Ḷ', // .l
+	45: 'Ḹ', // _.l
+
+	50: 'Ḏ', // _D
+	51: 'Ẏ', // .Y
+
+	60: 'ɱ', // \-/ (candrabindu)
+	61: '̮', // _ (ha_uk)
+	62: '^', // ^ (ext. sandhi)
+	63: "'", // avagraha
+	64: 'ɱ', // \_/ (candra e)
+	65: '/x', // \ (virama)
+	66: '…', // abbreviation
+	67: '’' // Latin apostrophe
+}
+
+// object with Balarama font characters
+var balarama = {
+	1: 'ä', // _a
+	2: 'é', // _i
+	3: 'ü', // _u
+	4: 'å', // .r
+	5: 'è', // _.r
+	6: 'ì', // 'n
+	7: 'ï', // ~n
+	8: 'ö', // .t
+	9: 'ò', // .d
+	10: 'ë', // .n
+	11: 'ç', // 's
+	12: 'ñ', // .s
+	13: 'à', // 'm (anusvara)
+	14: 'ù', // .h (visarga)
+	15: 'ÿ', // .l
+	16: 'û', // _.l
+
+	20: '.ò', // _d
+	21: 'ý', // .y
+
+	30: 'Ä', // _a
+	31: 'É', // _i
+	32: 'Ü', // _u
+	33: 'Å', // .r
+	34: 'È', // _.r
+	35: 'Ì', // 'n
+	36: 'Ï', // ~n
+	37: 'Ö', // .t
+	38: 'Ò', // .d
+	39: 'Ë', // .n
+	40: 'Ç', // 's
+	41: 'Ñ', // .s
+	42: 'À', // 'm (anusvara)
+	43: 'Ù', // .h (visarga)
+	44: 'ß', // .l
+	45: 'ß', // _.l
+
+	50: '.Ò', // _d
+	51: 'Ý', // .y
+
+	60: '~', // \-/ (candrabindu)
+	61: '_', // _ (ha_uk)
+
+	62: '^', // ^ (ext. sandhi)
+
+	63: "'", // avagraha
+	64: '~', // \_/ (candra e)
+	65: '/x', // \ (virama)
+	66: '…', // abbreviation
+	67: '’' // Latin apostrophe
+}
+
+// loops through each key in the 'balarama' object above and replaces with corresponding 'unicode'
+for (var key in balarama) { 
+
+    app.findGrepPreferences=app.changeGrepPreferences=null;
+    app.findGrepPreferences.findWhat=balarama[key];
+    app.changeGrepPreferences.changeTo=unicode[key];
+    app.activeDocument.changeGrep();
+};
---
+++ README.md
@@ -0,0 +1,31 @@
+# Indesign Balarama Font Character to Unicode Conversion Script
+
+This Indesign script will convert Balarama font characters 
+
+```c 
+Ï ï Ä ä É é Ç ç Ü ü Ò ò Ù ù ß ÿ À à Ì ì Ë ë Å å È è Ñ ñ Ö ö û 
+```
+
+into unicode compliant characters
+
+```c 
+Ṣ ṣ Ā ā Ī ī Ś ś Ū ū Ḍ ḍ Ḥ ḥ Ḷ ḷ Ṁ ṁ Ṅ ṅ Ṇ ṇ Ṛ ṛ Ṝ ṝ Ṣ ṣ Ṭ ṭ ḹ
+```
+
+## Installation
+
+>The easiest way to install this script into Indesign is to, within the software, open the ```Scripts Panel``` then ```right-click``` (Windows) or Control-click (macOS) a script and choose ```Reveal In Explorer``` (Windows) or ```Reveal In Finder``` (macOS).
+
+Then just copy the ```BalaramaToUnicode.jsx``` file here into the folder and it will be available to use. If, for whatever reason, you want to manually find the folder, it should be buried within a network of visible and hidden (by default) folders:
+
+ OS | Scripts Folder Location
+|--------------|:-----| 
+| macOS |  Users/```[username]```/Library/Preferences/Adobe InDesign/```[version]```/```[language]```/Scripts/Scripts Panel/
+| Windows |  Users\\```[username]```\AppData\Roaming\Adobe\InDesign\\```[version]```\\```[language]```\Scripts\Scripts Panel\
+
+## Use
+
+- Load document with Balarama font characters into Indesign
+- Open the Scripts Panel
+- Look in the ```User``` folder 
+- Double click ```BalaramaToUnicode``` (or ```right-click``` then ```Run Script```) 
